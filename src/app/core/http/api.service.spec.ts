e8d282e748a461718e8d1e8d5849f0858423e8b5
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-23 03:23:52 -0500

    test: fixing services tests

--- src/app/core/http/api.service.spec.ts
+++ src/app/core/http/api.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { ENVIRONMENT } from '../constants/tokens';
+import { Environment } from 'environments/environment';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ApiService', () => {
   let service: ApiService;
+  const env: Environment = {
+    production: false,
+    apiUrl: 'http://api.url:9999',
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: ENVIRONMENT, useValue: env }],
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ApiService);
   });
 
--- src/app/core/services/environment.service.spec.ts
+++ src/app/core/services/environment.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EnvironmentService } from './environment.service';
+import { Environment } from 'environments/environment';
+import { ENVIRONMENT } from '../constants/tokens';
 
 describe('EnvironmentService', () => {
   let service: EnvironmentService;
+  const env: Environment = {
+    production: false,
+    apiUrl: 'http://api.url:9999',
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: ENVIRONMENT, useValue: env }],
+    });
     service = TestBed.inject(EnvironmentService);
   });
 
