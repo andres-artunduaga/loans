5399c05be716034bdc90f44c672456d886245194
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-22 23:47:25 -0500

    chore: apply prettier format in base code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'znb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'loans';
