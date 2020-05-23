50fb4eac21c6619867ea2f393b5ff2a6a1815b1e
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-23 14:35:08 -0500

    feat: add header and logo components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 const materialModules = [
-  MatSidenavModule
+  MatSidenavModule,
+  MatToolbarModule
 ]
 
+const sharedComponents = [
+  HeaderComponent,
+  LogoComponent,
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [...sharedComponents],
   imports: [
     CommonModule, ...materialModules
   ],
   exports: [
-    ...materialModules
+    ...materialModules,
+    ...sharedComponents
   ]
 })
 export class SharedModule { }
