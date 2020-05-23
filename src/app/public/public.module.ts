a3b0e5be9c89736bdcf1a60e773ce111bfcfe706
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-23 13:55:46 -0500

    feat: add initial public layout

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PublicRoutingModule } from './public-routing.module';
-import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { PublicRoutingModule } from 'app/public/public-routing.module';
+import { PublicLayoutComponent } from 'app/public/public-layout/public-layout.component';
+import { SharedModule } from 'app/shared/shared.module';
 
 
 @NgModule({
   declarations: [PublicLayoutComponent],
   imports: [
     CommonModule,
-    PublicRoutingModule
-  ]
+    PublicRoutingModule,
+    SharedModule,
+  ],
 })
 export class PublicModule { }
