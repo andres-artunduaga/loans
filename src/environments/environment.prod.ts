34ae7a83e9e1b4674d7a8455bf78dc06dd9ac7c9
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-25 02:57:33 -0500

    chore: add initial amount in environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:3000"
+  apiUrl: "http://localhost:3000",
+  initialAmount: 1000000,
 };
 
 export type Environment = typeof environment;
