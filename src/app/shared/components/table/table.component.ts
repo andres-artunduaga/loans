56e9a703c52909e24abd3c0cacfefe260fc82921
Andres Artunduaga <andres.artunduaga@merlinjobs.com>
2020-05-25 14:15:31 -0500

    feat: make table responsive

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, TemplateRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input, TemplateRef, ChangeDetectorRef } from '@angular/core';
 import { ZNBTableFieldDefinition } from '@core/types/table.types';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'znb-table',
@@ -16,4 +17,16 @@ export class TableComponent {
   @Input() zeroStateIcon = 'error_outline';
   @Input() zeroStateMsg = 'No hay datos disponibles';
   @Input() bannerMsg = '';
+
+  mobileQuery: MediaQueryList;
+  // tslint:disable-next-line: variable-name
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 900px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change',this._mobileQueryListener);
+  }
+
+
 }
